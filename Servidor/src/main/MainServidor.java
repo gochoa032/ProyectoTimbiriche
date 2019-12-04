18bd62abf8f368c7c91d8c33d59c82dcead6df64
pc <pc@192.168.1.68>
2019-12-03 23:30:26 -0700

    Prueba

--- Servidor/src/main/MainServidor.java
+++ Servidor/src/main/MainServidor.java
@@ -17,7 +17,7 @@ public class MainServidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Servidor
+        //Servidor prueba
         Servidor servidor = new Servidor();
         servidor.escuchar();
     }
